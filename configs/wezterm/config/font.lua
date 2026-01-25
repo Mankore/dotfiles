8e518bb0358ff0a3093c9a04b817407e302653b3
Mankor <igorzhelezno@gmail.com>
2026-01-25 19:27:07 +0100

    fix(wezterm): Fix line-height font rendering issue, change font to monospace

--- configs/wezterm/config/font.lua
+++ configs/wezterm/config/font.lua
@@ -3,9 +3,9 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-	config.font = wezterm.font("CommitMono Nerd Font", { weight = "Regular" })
+	config.font = wezterm.font("CommitMono Nerd Font Mono", { weight = "Regular" })
 	config.font_size = 16.0
-	config.line_height = 1.3
+	config.line_height = 1.35
 
 	-- Enable ligatures
 	config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
