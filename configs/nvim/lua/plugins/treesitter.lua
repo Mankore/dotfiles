34e6b64b287ecfc7ad85248189bb5e8af611284f
Mankor <49029521+Mankore@users.noreply.github.com>
2026-02-10 09:04:26 +0100

    fix(nvim): fix treesitter config

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,51 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "html",
-        "lua",
-        "javascript",
-        "typescript",
-        "angular",
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-    },
-  },
+  "nvim-treesitter/nvim-treesitter",
+  config = function()
+    local parsers = {
+      "lua",
+      "luadoc",
+      "typescript",
+      "javascript",
+      "tsx",
+      "jsx",
+      "angular",
+      "html",
+      "html_tags",
+      "json",
+      "bash",
+      "diff",
+      "markdown",
+      "markdown_inline",
+      "vim",
+    }
+    require("nvim-treesitter").install(parsers)
+
+    local filetypes = {
+      "lua",
+      "luadoc",
+      "typescript",
+      "javascript",
+      "tsx",
+      "jsx",
+      "angular",
+      "typescriptreact",
+      "html",
+      "html_tags",
+      "json",
+      "bash",
+      "diff",
+      "markdown",
+      "markdown_inline",
+      "vim",
+    }
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function()
+        print("Starting treesitter for " .. vim.bo.filetype)
+        vim.treesitter.start()
+      end,
+    })
+  end,
 }
