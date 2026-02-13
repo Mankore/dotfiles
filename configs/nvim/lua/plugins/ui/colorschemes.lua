44021b125b4a99b847fbd9483c3f1d7c54ca01cb
Mankor <49029521+Mankore@users.noreply.github.com>
2026-02-13 16:36:25 +0100

    feat: Add kanagawa colorscheme

--- configs/nvim/lua/plugins/ui/colorschemes.lua
+++ configs/nvim/lua/plugins/ui/colorschemes.lua
@@ -10,7 +10,40 @@ return {
       vim.g.moonflyTransparent = false
       vim.g.moonflyNormalFloat = true
 
-      vim.cmd.colorscheme("moonfly")
+      -- vim.cmd.colorscheme("moonfly")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave", -- Load "wave" theme
+        background = { -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus",
+        },
+      })
+
+      vim.cmd.colorscheme("kanagawa-dragon")
     end,
   },
 }
