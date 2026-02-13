75c24ff0e509a9dfcc8d51ac341e7af7c5fbb2a0
Mankor <igorzhelezno@gmail.com>
2026-02-13 22:29:01 +0100

    feat: Add nordic / cyberdream colorschemes

--- configs/nvim/lua/plugins/ui/colorschemes.lua
+++ configs/nvim/lua/plugins/ui/colorschemes.lua
@@ -18,39 +18,23 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require("kanagawa").setup({
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = "wave", -- Load "wave" theme
-        background = { -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
-
       vim.cmd.colorscheme("kanagawa-dragon")
     end,
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- vim.cmd("colorscheme rose-pine")
+      -- vim.cmd.colorscheme("cyberdream")
     end,
   },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- require('nordic').load()
+    end
+  }
 }
