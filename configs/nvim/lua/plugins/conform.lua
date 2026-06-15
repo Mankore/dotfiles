813bfe01ac167509cbe1fbbba2daa089fb47626f
Mankor <49029521+Mankore@users.noreply.github.com>
2026-06-15 09:06:01 +0200

    refactor(nvim): Disable format_on_save, add yamlfmt configuration

--- configs/nvim/lua/plugins/conform.lua
+++ configs/nvim/lua/plugins/conform.lua
@@ -16,25 +16,32 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = "fallback",
-          }
-        end
-      end,
+      -- format_on_save = function(bufnr)
+      --   -- Disable "format_on_save lsp_fallback" for languages that don't
+      --   -- have a well standardized coding style. You can add additional
+      --   -- languages here or re-enable it for the disabled ones.
+      --   local disable_filetypes = { c = true, cpp = true }
+      --   if disable_filetypes[vim.bo[bufnr].filetype] then
+      --     return nil
+      --   else
+      --     return {
+      --       timeout_ms = 500,
+      --       lsp_format = "fallback",
+      --     }
+      --   end
+      -- end,
       formatters_by_ft = {
         go = { "goimports", "gofumpt" },
         lua = { "stylua" },
+        yaml = { "yamlfmt" },
         ["_"] = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        yamlfmt = {
+          command = "yamlfmt",
+          args = { "-formatter", "retain_line_breaks=true" },
+        },
+      }
     },
   },
 }
